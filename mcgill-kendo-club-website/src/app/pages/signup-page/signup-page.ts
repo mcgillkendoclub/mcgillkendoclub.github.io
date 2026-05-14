05ed37d2fe98d204fc3402c94b47863762efeb57
Bong <songning.lan@gmail.com>
2026-05-14 03:13:06 -0400

    cleanup before deployment

--- mcgill-kendo-club-website/src/app/pages/signup-page/signup-page.ts
+++ mcgill-kendo-club-website/src/app/pages/signup-page/signup-page.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ChangeDetectorRef, inject } from '@angular/core';
 import { TranslatePipe } from '@ngx-translate/core';
 import { GeneralHeader } from "@app/components/general-header/general-header";
 import { GeneralFooter } from "@app/components/general-footer/general-footer";
@@ -14,4 +14,25 @@ import { PricingCardInfo } from '@app/interfaces/pricing-card-info';
 })
 export class SignupPage {
   readonly pricingCards: PricingCardInfo[] = data.membershipOptions;
+  readonly paymentEmail: string = 'mcgillkendoclub@gmail.com';
+  emailCopied: boolean = false;
+  emailFading: boolean = false;
+  private copyTimer: ReturnType<typeof setTimeout> | null = null;
+  private cdr = inject(ChangeDetectorRef);
+
+  copyEmail() {
+    navigator.clipboard.writeText(this.paymentEmail);
+    this.emailCopied = true;
+    this.emailFading = false;
+    if (this.copyTimer) clearTimeout(this.copyTimer);
+    this.copyTimer = setTimeout(() => {
+      this.emailFading = true;
+      this.cdr.markForCheck();
+      setTimeout(() => {
+        this.emailCopied = false;
+        this.emailFading = false;
+        this.cdr.markForCheck();
+      }, 400);
+    }, 1600);
+  }
 }
